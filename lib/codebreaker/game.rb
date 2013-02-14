5fa936946e016a91f66b07f76db16809f4a043ba
gekken <pbomars@gmail.com>
2013-02-14 00:26:23 -0800

    housekeeping

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -4,9 +4,19 @@ module Codebreaker
       @output = output
     end
 
-    def start
+    def start(secret)
+      @secret = secret
       @output.puts "Welcome to Codebreaker!"
       @output.puts "Enter guess:"
     end
+
+    def guess(guess)
+      if @secret.include?(guess[0])
+        @output.puts('-')
+      else
+        @output.puts('')
+      end
+    end
+
   end
 end
